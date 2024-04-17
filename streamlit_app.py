21f744046be9f552e107cb8381a2573b2f52e558
Suvadeep Datta <34707807+Suvadeepgm@users.noreply.github.com>
2024-04-17 03:35:48 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,16 +32,16 @@ def get_response(prompt, model="claude-3-haiku-20240307"):
 def setup_sideBar():
     st.sidebar.header('About')
     st.sidebar.markdown("""
-        App is created using [OpenAI](https://openai.com) GPT api and 🎈[Streamlit](https://streamlit.io/).
+        App is created using [Anthropic](https://www.anthropic.com/) Claude - Haiku api and 🎈[Streamlit](https://streamlit.io/).
         """)
     st.sidebar.markdown("""
-        Developed by [Suvadeep Datta](https://www.linkedin.com/in/rifatmonzur/)
+        Developed by [Suvadeep Datta](https://www.linkedin.com/in/connectsuvadeep/)
         """)
 
     st.sidebar.header("Resources")
     st.sidebar.markdown(
         """
-        - [Source Code](https://github.com/rifat1234/Prompt-To-Do-Common-Tasks)
+        - [Source Code](https://github.com/Suvadeepgm/InsuranceAssist)
         """)
 
 def create_ui():
@@ -64,18 +64,19 @@ def create_ui():
     #option = st.selectbox(
         #'Choose Task',
         #(task_proofread, task_summarise))
-    name = st.text_area(f'Write your name', height=150,
-                       value="", max_chars=10000)
+    name = st.text_area(f'Write your name', height=50,
+                       value="", max_chars=100)
 
-    age = st.text_area(f'Write your age', height=150,
-                       value="", max_chars=10000)
+    age = st.number_input(f'Write your age', format=%i ,
+                       value="", max_chars=100)
 
-    gender = st.text_area(f'Write your gender', height=150,
-                       value="", max_chars=10000)
-    occupation = st.text_area(f'What is your employment status??', height=150,
-                       value="", max_chars=10000)
-    smoking_history = st.text_area(f'Do you smoke?', height=150,
-                       value="", max_chars=10000)
+    gender = st.text_area(f'Write your gender', height=50,
+                       value=None, max_chars=100)
+    occupation = st.text_area(f'What is your employment status?', height=50,
+                       value="", max_chars=1000)
+    smoking_history = st.selectbox(
+...                    'Are you a smoker or a non-smoker?',
+...                     ('Smoker', 'Non - Smoker'))
 
     submitted = st.button('Submit')
 
@@ -85,9 +86,31 @@ def create_ui():
             return
         
         prompt = f"Based on the provided name {name} , age {age} and Gender {gender}, smoking history {smoking_history} , Occupation {occupation} generate a brief profile summarizing key details that would be relevant for selecting a life insurance policy."
-        response = get_response(prompt)
-        st.text_area('Initial Profile', height=150, value=response, disabled=True)
+        initial_profile = get_response(prompt)
+        st.text_area('Initial Profile', height=550, value=initial_profile, disabled=True)
         #add_copy(response)
+
+        insurance_type = st.selectbox(
+...                    'Select the type of insurance you would like to enquire about:',
+...                     ('Life', 'Health', 'Automobile', 'Home'))
+
+        submitted = st.button('Submit')
+        if submitted:
+            prompt = f"Given the profile: {initial_profile} and the requested insurance type {insurance_type}, suggest 2-3 specific policy options of John Hancock that could be a good fit, along with a brief explanation for each recommendation. Give the names of the plans as well."
+            policy_recommendation = get_response(prompt)
+            st.text_area('Policy Recommendation', height=550, value=policy_recommendation, disabled=True)
+
+            quote_yes_no = st.selectbox(
+...                    'Do you want to see the approximate quotes for the above policies?',
+...                     ('Yes', 'No'))
+            if quote_yes_no='Yes':
+                prompt = f"Given the profile: {initial_profile} and the Policy Recommendation {policy_recommendation} , suggest a Quote for the recommended policies."
+                quote_recommendation = get_response(prompt)
+                st.text_area('Apptoximate Quotes', height=550, value=quote_recommendation, disabled=True)
+
+            
+
+
 
 client = setup_anthropic()
 create_ui()
